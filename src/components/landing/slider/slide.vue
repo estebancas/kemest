5582dd52c01b6c54b86078e68f6bc89fd7391d53
estebancas <estcascor94@hotmail.com>
2019-05-27 18:29:39 -0600

    added animation and effects on slide

--- src/components/landing/slider/slide.vue
+++ src/components/landing/slider/slide.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="slider-card">
-    <div class="picture-overlay">
+    <div class="picture-holder">
       <a>
-        <img
-        :src="img"
-        draggable="false"
-        @mousedown="triggerStartDrag($event)"
-        @mouseup="triggerStopDrag($event)"
-        >
+        <figure>
+          <img
+            :src="img"
+            draggable="false"
+            @mousedown="triggerStartDrag($event)"
+            @mouseup="triggerStopDrag($event)"
+          >
+        </figure>
       </a>
+      <div class="picture-overlay text-center">
+        QUICK VIEW
+      </div>
     </div>
     <div class="item-description pt-2 d-flex">
-      <label class="item-name m-0">{{name}}</label>
-      <label class="item-price m-0">${{price}}</label>
+      <b-row>
+        <b-col sm="12" class="text-left">
+          <label class="item-name m-0">{{name}}</label>
+        </b-col>
+        <b-col sm="12" class="text-left">
+          <label class="item-price m-0">${{price}}</label>
+        </b-col>
+      </b-row>
     </div>
   </div>
 </template>
@@ -52,27 +63,55 @@ export default {
   margin-right: 30px;
 }
 
-.slider-card img {
-  width: 100%;
+.picture-holder {
+  height: 90%;
+  position: relative;
+}
+
+.picture-holder:hover .picture-overlay {
+  height: 6%;
+  padding: 5px;
 }
 
 .picture-overlay {
-    height: 90%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: #f9f9f9;
+  cursor: pointer;
+  height: 0;
+  overflow: hidden;
+  transition: .5s ease;
 }
 
-.picture-overlay a {
-    width: 100%;
-    height: 100%;
-    display: block;
+.picture-holder a {
+  width: 100%;
+  height: 100%;
+  display: block;
 }
 
-.picture-overlay a img {
-    width: 100%;
-    height: 100%;
+.picture-holder a figure {
+  margin: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
 }
 
-.item-description {
+.picture-holder a img {
+  width: 100%;
+  height: 100%;
 
+  /*zoom in animation*/
+  -webkit-transform: scale(1);
+  transform: scale(1);
+  -webkit-transition: 0.3s ease-in-out;
+  transition: 0.3s ease-in-out;
+}
+
+.picture-holder:hover a img {
+  -webkit-transform: scale(1.3);
+  transform: scale(1.3);
 }
 </style>
 
