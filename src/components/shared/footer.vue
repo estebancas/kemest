02d3fd75b8b4be1932ada5dc7f581f3b149775a8
Esteban Castro <Esteban.Castro@intertecintl.com>
2019-06-21 09:09:36 -0600

    footer options

--- src/components/shared/footer.vue
+++ src/components/shared/footer.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="footer mt-5 py-5">
     <b-container fluid>
-      <b-row class="justify-content-center my-3">
-        <b-col sm="4" class="text-left" px-5>
-            <h6 class="text-left mb-4">JOIN OUR NEWSLETTER</h6>
-            <b-form-input type="email" placeholder="Enter your email here*"></b-form-input>
-            <FlatButton class="mt-3 mb-4" text="Subscribe Now"/>
-            <label class="mt-5">&copy;2019 by Phantom.creative</label>
+      <b-row class="justify-content-center my-3 main-row">
+        <b-col sm="4" class="text-left px-5">
+          <h6 class="text-left mb-4">JOIN OUR NEWSLETTER</h6>
+          <b-form-input type="email" placeholder="Enter your email here*"></b-form-input>
+          <FlatButton class="mt-3 mb-4" text="Subscribe Now"/>
+          <label class="mt-5 footer-creator-label">&copy;2019 by Phantom.creative</label>
         </b-col>
-        <b-col sm="4" px-5>
+        <div class="hr-holder">
+          <div class="hr"></div>
+        </div>
+        <b-col sm="4 px-5">
           <b-row class="pt-3 mt-4">
-            <b-col sm="4">1</b-col>
-            <b-col sm="4">2</b-col>
-            <b-col sm="4">3</b-col>
+            <b-col sm="4">
+              <b-row class="text-left">
+                <b-col sm="12"><label>Stockists</label></b-col>
+                <b-col sm="12"><label>About</label></b-col>
+                <b-col sm="12"><label>Contact</label></b-col>
+              </b-row>
+            </b-col>
+            <b-col sm="4">
+              <b-row class="text-left">
+                <b-col sm="12"><label>FAQ</label> </b-col>
+                <b-col sm="12"><label>Shipping & returns</label></b-col>
+                <b-col sm="12"><label>Store Policy</label></b-col>
+              </b-row>
+            </b-col>
+            <b-col sm="4">
+              <b-row class="text-left">
+                <b-col sm="12"><label>Facebook</label></b-col>
+                <b-col sm="12"><label>Instagram</label></b-col>
+                <b-col sm="12"><label>Pinterest</label></b-col>
+              </b-row>
+            </b-col>
           </b-row>
         </b-col>
       </b-row>
@@ -21,7 +42,7 @@
 </template>
 
 <script>
-import FlatButton from '../shared/button/flatButton'
+import FlatButton from "../shared/button/flatButton";
 
 export default {
   name: "Footer",
@@ -35,10 +56,26 @@ export default {
 <style lang="scss" scoped>
 .footer {
   border-top: 1px solid;
-  label {
+  .footer-creator-label {
     color: #555555;
     font-weight: 100;
     font-family: monospace;
+  }
+  label {
+    cursor: pointer;
+  }
+  .main-row {
+    position: relative;
+  }
+  .hr-holder {
+    position: absolute;
+    height: 70%;
+    width: 0;
+    max-width: 0;
+    .hr {
+      border-left: 1px solid;
+      height: 100%;
+    }
   }
 }
 </style>
