2d9bc1669ff787e33712f0d96a5746cf5d5da242
Esteban Castro <Esteban.Castro@intertecintl.com>
2019-06-27 16:40:17 -0600

    footer for mobile device

--- src/components/shared/footer.vue
+++ src/components/shared/footer.vue
@@ -1,42 +1,61 @@
 <template>
-  <div class="footer mt-5 py-5">
+  <div class="footer mt-5 py-5 mb-5 mb-md-0">
     <b-container fluid>
       <b-row class="justify-content-center my-3 main-row">
-        <b-col sm="4" class="text-left px-5">
-          <h6 class="text-left mb-4">JOIN OUR NEWSLETTER</h6>
+        <b-col sm="4" class="text-left px-3 px-md-5">
+          <h6 class="text-center text-md-left mb-4">JOIN OUR NEWSLETTER</h6>
           <b-form-input type="email" placeholder="Enter your email here*"></b-form-input>
           <FlatButton class="mt-3 mb-4" text="Subscribe Now"/>
-          <label class="mt-5 footer-creator-label">&copy;2019 by Phantom.creative</label>
+          <label class="mt-5 d-none d-md-block footer-creator-label">&copy;2019 by Phantom.creative</label>
         </b-col>
-        <div class="hr-holder">
+        <div class="hr-holder d-none d-md-block">
           <div class="hr"></div>
         </div>
-        <b-col sm="4 px-5">
+        <b-col sm="4 px-3 px-md-5">
           <b-row class="pt-3 mt-4">
-            <b-col sm="4">
+            <b-col cols="4">
               <b-row class="text-left">
-                <b-col sm="12"><label>Stockists</label></b-col>
-                <b-col sm="12"><label>About</label></b-col>
-                <b-col sm="12"><label>Contact</label></b-col>
+                <b-col sm="12">
+                  <label>Stockists</label>
+                </b-col>
+                <b-col sm="12">
+                  <label>About</label>
+                </b-col>
+                <b-col sm="12">
+                  <label>Contact</label>
+                </b-col>
               </b-row>
             </b-col>
-            <b-col sm="4">
+            <b-col cols="4">
               <b-row class="text-left">
-                <b-col sm="12"><label>FAQ</label> </b-col>
-                <b-col sm="12"><label>Shipping & returns</label></b-col>
-                <b-col sm="12"><label>Store Policy</label></b-col>
+                <b-col sm="12">
+                  <label>FAQ</label>
+                </b-col>
+                <b-col sm="12">
+                  <label>Shipping & returns</label>
+                </b-col>
+                <b-col sm="12">
+                  <label>Store Policy</label>
+                </b-col>
               </b-row>
             </b-col>
-            <b-col sm="4">
+            <b-col cols="4">
               <b-row class="text-left">
-                <b-col sm="12"><label>Facebook</label></b-col>
-                <b-col sm="12"><label>Instagram</label></b-col>
-                <b-col sm="12"><label>Pinterest</label></b-col>
+                <b-col sm="12">
+                  <label>Facebook</label>
+                </b-col>
+                <b-col sm="12">
+                  <label>Instagram</label>
+                </b-col>
+                <b-col sm="12">
+                  <label>Pinterest</label>
+                </b-col>
               </b-row>
             </b-col>
           </b-row>
         </b-col>
       </b-row>
+      <label class="text-left mt-5 d-block d-md-none footer-creator-label">&copy;2019 by Phantom.creative</label>
     </b-container>
   </div>
 </template>
